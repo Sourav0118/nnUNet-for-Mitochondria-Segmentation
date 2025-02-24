7d84dee2d29672166cf49e697ea7e9c9b3af872c
Sourav Saini <127005716+Sourav0118@users.noreply.github.com>
2025-02-24 17:45:51 +0530

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
-# Clone nnUNet repository
-git clone https://github.com/MIC-DKFZ/nnUNet.git
-cd nnUNet
-pip install -e .
 
 # Set dataset paths
 export nnUNet_raw="/content/nnUNet_raw"
 export nnUNet_preprocessed="/content/nnUNet_preprocessed"
 export nnUNet_results="/content/drive/MyDrive/nnUNet_results"
-
-# Preprocess dataset
-nnUNetv2_plan_and_preprocess -d 1 --verify_dataset_integrity
 
 # Train the model
 # add --c if you want to continue the training
